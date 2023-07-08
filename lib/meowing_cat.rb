f011df6ec774f7fe8c099bfe13bb7ba8208f6bba
reaganmunene <reagannmunene@gmail.com>
2023-07-08 20:37:26 +0300

    completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+require 'pry'
+### code your solution here.
+class Cat
+
+  attr_accessor :name #name is the attribute here
+
+ def meow
+   puts "meow!"
+ end
+
+
+
+end
+binding.pry
